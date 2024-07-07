372a57553301b258237b8f22782608be275be326
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-07-07 13:30:21 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -1,36 +1,15 @@
-def get_soundex_code(c):
-    c = c.upper()
-    mapping = {
-        'B': '1', 'F': '1', 'P': '1', 'V': '1',
-        'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
-        'D': '3', 'T': '3',
-        'L': '4',
-        'M': '5', 'N': '5',
-        'R': '6'
-    }
-    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
+soundex = name.upper()
+prev_code = get_soundex_code(soundex)
  
-def generate_soundex(name):
-    if not name:
-        return ""
-    # Start with the first letter (capitalized)
-    soundex = name[0].upper()
-    prev_code = get_soundex_code(soundex)
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
-        if len(soundex) == 4:
-            break
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
-    return soundex
+for char in name[1:]:
+    code = get_soundex_code(char)
+    if code != '0' and code != prev_code:
+        soundex += code
+        prev_code = code
+    if len(soundex) == 4:
+        break
  
-# Test the function
-print(generate_soundex("Smith"))  # Output should be "S530"
-print(generate_soundex("Smythe")) # Output should be "S530"
-print(generate_soundex("Robert")) # Output should be "R163"
-print(generate_soundex("Rupert")) # Output should be "R163"
-print(generate_soundex("Ashcraft")) # Output should be "A261"
-print(generate_soundex("Ashcroft")) # Output should be "A261"
+# Pad with zeros if necessary
+soundex = soundex.ljust(4, '0')
+ 
+return soundex
