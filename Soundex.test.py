bfb9611e75c958f6acd003007900e88449b67ec4
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-07-07 13:18:33 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,14 +1,20 @@
 import unittest
 from Soundex import generate_soundex
-
+ 
 class TestSoundex(unittest.TestCase):
 
-    def test_empty_string(self):
-        self.assertEqual(generate_soundex(""), "")
+    def test_case_insensitivity(self):
+        self.assertEqual(generate_soundex("VAISHNAVI"), generate_soundex("vaiSHNavi"))
+ 
+    def test_vowels(self):
+        self.assertEqual(generate_soundex("Aeiou"), "I185")
+ 
+    def test_consonants(self):
+        self.assertEqual(generate_soundex("Bcdghjklmnpqrstvwxyz"), "V145")
 
-    def test_single_character(self):
-        self.assertEqual(generate_soundex("A"), "A000")
-
-    
+ def test_single_character(self):
+        self.assertEqual(generate_soundex("V"), "00V0")
+        self.assertEqual(generate_soundex("S"), "0S00")
+ 
 if __name__ == '__main__':
     unittest.main()
