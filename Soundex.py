6fea6828ac1228ef314d8351147e4e02f795d015
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-07-07 15:09:48 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -2,27 +2,17 @@ def get_soundex_code(char):
     """
     Returns the Soundex code for the given character.
     """
-    mapping = {
+    return {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
         'D': '3', 'T': '3',
         'L': '4',
         'M': '5', 'N': '5',
         'R': '6'
-    }
-    return mapping.get(char.upper(), '0')
+    }.get(char.upper(), '0')
  
 def generate_soundex(name):
     """
     Generates the Soundex code for the given name.
     """
-    if not name:
-        return ""
- 
-    soundex = name[0].upper()
-    for char in name[1:]:
-        code = get_soundex_code(char)
-        if code != '0':
-            soundex += code
- 
-    return soundex.ljust(4, '0')
+    return name[0].upper() + ''.join(get_soundex_code(char) for char in name[1:])[:3].ljust(4, '0')
