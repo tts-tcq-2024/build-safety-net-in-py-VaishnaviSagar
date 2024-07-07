6f372e918e2ab61af786abed25a79a66af4b5f8f
VaishnaviSagar <67277305+VaishnaviSagar@users.noreply.github.com>
2024-07-07 15:13:46 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -1,14 +1,41 @@
 import unittest
-from Soundex import generate_soundex
+from Soundex import get_soundex_code, generate_soundex
+ 
 class TestSoundex(unittest.TestCase):
-    def test_single_character(self):
-        self.assertEqual(generate_soundex("V"), "00V0")
-        self.assertEqual(generate_soundex("S"), "0S00")
-    def test_case_insensitivity(self):
-        self.assertEqual(generate_soundex("VAISHNAVI"), generate_soundex("vaiSHNavi"))
-    def test_vowels(self):
-        self.assertEqual(generate_soundex("Aeiou"), "I185")
-    def test_consonants(self):
-        self.assertEqual(generate_soundex("Bcdghjklmnpqrstvwxyz"), "V145")
+    def test_get_soundex_code(self):
+        """Test the get_soundex_code function"""
+        self.assertEqual(get_soundex_code('B'), '1')
+        self.assertEqual(get_soundex_code('F'), '1')
+        self.assertEqual(get_soundex_code('P'), '1')
+        self.assertEqual(get_soundex_code('V'), '1')
+        self.assertEqual(get_soundex_code('C'), '2')
+        self.assertEqual(get_soundex_code('G'), '2')
+        self.assertEqual(get_soundex_code('J'), '2')
+        self.assertEqual(get_soundex_code('K'), '2')
+        self.assertEqual(get_soundex_code('Q'), '2')
+        self.assertEqual(get_soundex_code('S'), '2')
+        self.assertEqual(get_soundex_code('X'), '2')
+        self.assertEqual(get_soundex_code('Z'), '2')
+        self.assertEqual(get_soundex_code('D'), '3')
+        self.assertEqual(get_soundex_code('T'), '3')
+        self.assertEqual(get_soundex_code('L'), '4')
+        self.assertEqual(get_soundex_code('M'), '5')
+        self.assertEqual(get_soundex_code('N'), '5')
+        self.assertEqual(get_soundex_code('R'), '6')
+        self.assertEqual(get_soundex_code('a'), '1')
+        self.assertEqual(get_soundex_code('z'), '2')
+        self.assertEqual(get_soundex_code('1'), '0')
+ 
+    def test_generate_soundex(self):
+        """Test the generate_soundex function"""
+        self.assertEqual(generate_soundex(""), "")
+        self.assertEqual(generate_soundex("A"), "A000")
+        self.assertEqual(generate_soundex("Smith"), "S530")
+        self.assertEqual(generate_soundex("Smythe"), "S530")
+        self.assertEqual(generate_soundex("Robert"), "R163")
+        self.assertEqual(generate_soundex("Rupert"), "R163")
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
+ 
 if __name__ == '__main__':
     unittest.main()
